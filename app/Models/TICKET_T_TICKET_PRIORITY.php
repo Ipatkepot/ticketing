fe7d070c555a1555a3457cb3751cfbf15bd281a6
keul <muhammadalham1412@gmail.com>
2026-04-15 14:22:06 +0700

    rename

--- app/Models/TICKET_T_TICKET_PRIORITY.php
+++ app/Models/TICKET_T_TICKET_PRIORITY.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TicketPriority extends Model
 {
-    
+    protected $table = 'TICKET_T_TICKET_PRIORITY';
     protected $fillable = [
         'name',
     ];
