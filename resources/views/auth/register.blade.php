9c7208c28e01682c10d13b1f72e8b0d3f77e9e77
keul <muhammadalham1412@gmail.com>
2026-04-17 10:26:30 +0700

    update regis

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,66 +1,64 @@
-@extends('layouts.guest')
+<x-guest-layout>
+    <div class="card bg-secondary shadow border-0">
+        <div class="card-body px-lg-5 py-lg-5">
+            <div class="text-center text-muted mb-4">
+                <small>{{ __('Daftar akun anyar') }}</small>
+            </div>
+            <form method="POST" action="{{ route('register') }}">
+                @csrf
 
-@section('content')
-<div class="row justify-content-center">
-    <div class="col-lg-6 col-md-8">
-        <div class="card bg-secondary shadow border-0">
-            <div class="card-body px-lg-5 py-lg-5">
-                <div class="text-center text-muted mb-4">
-                    <small>Daptar make email & password</small>
+                <div class="form-group mb-3">
+                    <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                        </div>
+                        <input id="name" class="form-control" placeholder="Name" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                    </div>
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </div>
-                <form method="POST" action="{{ route('register') }}">
-                    @csrf
 
-                    <div class="form-group">
-                        <div class="input-group input-group-alternative mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
-                            </div>
-                            <input class="form-control" placeholder="Nami Lengkap" type="text" name="name" value="{{ old('name') }}" required autofocus>
+                <div class="form-group mb-3">
+                    <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-email-83"></i></span>
                         </div>
-                        @error('name') <small class="text-danger">{{ $message }}</small> @enderror
+                        <input id="email" class="form-control" placeholder="Email" type="email" name="email" :value="old('email')" required autocomplete="username" />
                     </div>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-                    <div class="form-group">
-                        <div class="input-group input-group-alternative mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                            </div>
-                            <input class="form-control" placeholder="Email" type="email" name="email" value="{{ old('email') }}" required>
+                <div class="form-group mb-3">
+                    <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
                         </div>
-                        @error('email') <small class="text-danger">{{ $message }}</small> @enderror
+                        <input id="password" class="form-control" placeholder="Password" type="password" name="password" required autocomplete="new-password" />
                     </div>
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
 
-                    <div class="form-group">
-                        <div class="input-group input-group-alternative">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                            </div>
-                            <input class="form-control" placeholder="Password" type="password" name="password" required>
+                <div class="form-group">
+                    <div class="input-group input-group-alternative">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
                         </div>
-                        @error('password') <small class="text-danger">{{ $message }}</small> @enderror
+                        <input id="password_confirmation" class="form-control" placeholder="Confirm Password" type="password" name="password_confirmation" required autocomplete="new-password" />
                     </div>
+                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                </div>
 
-                    <div class="form-group mt-3">
-                        <div class="input-group input-group-alternative">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                            </div>
-                            <input class="form-control" placeholder="Konfirmasi Password" type="password" name="password_confirmation" required>
-                        </div>
-                    </div>
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary my-4">
+                        {{ __('Register') }}
+                    </button>
+                </div>
 
-                    <div class="text-center">
-                        <button type="submit" class="btn btn-primary mt-4">Jieun Akun</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-        <div class="row mt-3">
-            <div class="col-12 text-center">
-                <a href="{{ route('login') }}" class="text-light"><small>Geus boga akun? Login didiyeu</small></a>
-            </div>
+                <div class="flex items-center justify-center mt-2">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
+                </div>
+            </form>
         </div>
     </div>
-</div>
-@endsection
+</x-guest-layout>
